226d89f801a096aaae66566ab11be54bc77e264b
Xabi <dm3-1-07.17@uni.eus>
2018-03-09 12:17:40 +0100

    -lehenengo egoera

---
+++ Proiektua/Proiektua/Proiektua.py
@@ -0,0 +1,18 @@
+'''
+Created on 7 mar. 2018
+
+@author: DM3-1-07
+'''
+
+
+class Persona(object):
+   
+
+
+    def __init__(self, dni, nombre, apellidos, direccion, telefono, email):
+        self.dni = dni
+        self.nombre = nombre
+        self.apellidos = apellidos
+        self.direccion = direccion
+        self.telefono = telefono
+        self.email = email
