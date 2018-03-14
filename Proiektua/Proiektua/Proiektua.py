e8b59e6a0547e7146ae28924042f2d72428ec53d
Xabi <dm3-1-07.17@uni.eus>
2018-03-14 12:44:12 +0100

    Imprimir nota

--- Proiektua/Proiektua/Proiektua.py
+++ Proiektua/Proiektua/Proiektua.py
@@ -17,20 +17,32 @@ class Persona(object):
         self.telefono = telefono
         self.email = email
         print('')
-
+ 
 class Profesor(Persona):
     def __init__(self,departamento):
         self.departamento = departamento
-        
+    def bistaratu(self):
+        print(self.__dict__)    
 class Alumno(Persona):
     def __init__(self,curso):
         self.curso = curso
-        
+    def bistaratu(self):
+        print(self.__dict__)    
 class Asignatura:
     def __init__(self,nombre, descripcion):
         self.nombre = nombre
         self.descripcion = descripcion
+    def bistaratu(self):
+        print(self.__dict__)
+
 class Nota:
     def __init__(self, total, media):
         self.total = total
         self.media = media
+    def bistaratu(self):
+        print(self.__dict__)
+
+A1 = Asignatura('a','b')
+A1.bistaratu()
+N1=Nota(8,7)
+N1.bistaratu()
