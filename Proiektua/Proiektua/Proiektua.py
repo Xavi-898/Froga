8a897d1843caf579d612e05b8e611bcd12560d5d
Xabi <dm3-1-07.17@uni.eus>
2018-03-23 11:59:58 +0100

    --konfliktoa kendu

--- Proiektua/Proiektua/Proiektua.py
+++ Proiektua/Proiektua/Proiektua.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 '''
 Created on 7 mar. 2018
 
@@ -46,61 +45,8 @@ class Nota:
 A1 = Asignatura('a','b')
 A1.bistaratu()
 N1=Nota(8,7)
-<<<<<<< HEAD
+
 print('aldaketa')
 N1.bistaratu()
-=======
 N1.bistaratu()
 print('kuku')
->>>>>>> branch 'master' of https://github.com/Xavi-898/Froga.git
-=======
-'''
-Created on 7 mar. 2018
-
-@author: DM3-1-07
-'''
-
-
-class Persona(object):
-   
-
-
-    def __init__(self, dni, nombre, apellidos, direccion, telefono, email):
-        self.dni = dni
-        self.nombre = nombre
-        self.apellidos = apellidos
-        self.direccion = direccion
-        self.telefono = telefono
-        self.email = email
-        print('')
- 
-class Profesor(Persona):
-    def __init__(self,departamento):
-        self.departamento = departamento
-    def bistaratu(self):
-        print(self.__dict__)     
-class Alumno(Persona):
-    def __init__(self,curso):
-        self.curso = curso
-    def bistaratu(self):
-        print(self.__dict__)        
-class Asignatura:
-    def __init__(self,nombre, descripcion):
-        self.nombre = nombre
-        self.descripcion = descripcion
-    def bistaratu(self):
-        print(self.__dict__)
-
-class Nota:
-    def __init__(self, total, media):
-        self.total = total
-        self.media = media
-    def bistaratu(self):
-        print(self.__dict__)
-
-A1 = Asignatura('a','b')
-A1.bistaratu()
-N1=Nota(8,7)
-N1.bistaratu()
-
->>>>>>> branch 'master' of https://github.com/Xavi-898/Froga.git
